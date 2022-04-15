216c4710a0e4915e6f735d3c689092dbe3a65d1b
Mjumbe Poe <mjumbewu@gmail.com>
2022-04-15 00:26:39 -0400

    Update comments for the server and generate Docco documentation

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-// Read/Write API for Trail Waze
-// =============================
+// Read/Write API for "Trail Waze" App
+// ===================================
 //
 // This API is implemented using the Express JS micro-framework, and the Knex JS
 // SQL-generating library. It is a REST-style API and supports two primary
 // operations: (1) retrieving a list of recent issues reported (within the last
 // 30 days), and (2) submitting a new issue.
 //
-// (Note that Cross-Origin Resource Sharing, or CORS, is not something you have
-//  to be very familiar with, except to know that it is a security feature of
-//  HTTP. Suffice to say that including `app.use(cors())` is something you'll
-//  often have to do. https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS).
+// (note that _Cross-Origin Resource Sharing_, or _CORS_, is not something you
+// have to be very familiar with, except to know that it is a security feature
+// of HTTP. Suffice to say that including `app.use(cors())` is something you'll
+// often have to do. https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS).
 
 const express = require('express');
 const knex = require('knex');
@@ -28,12 +28,12 @@ const db = knex({
 
 
 // Serialization and Deserialization
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ---------------------------------
 //
 // HTTP APIs that exchange data with a client need to be able to **serialize**
 // and **deserialize** data.
-//
-// * Serialization is the process of converting data that comes from, say, a
+
+// * **Serialization** is the process of converting data that comes from, say, a
 //   database into a format that's ready to be sent to the client. In this case,
 //   we're creating a `recordToFeature` function to convert a database record as
 //   we would get from Knex into a GeoJSON feature. Sometimes "serializing"
@@ -55,9 +55,9 @@ const recordToFeature = function (record) {
   };
 };
 
-// * Deserialization is the process of converting data that comes from a client
-//   into something useable be your system. In this case, we're creating a
-//   `featureToRecord` function to convert a GeoJSON feature that we get from
+// * **Deserialization** is the process of converting data that comes from a
+//   client into something useable be your system. In this case, we're creating
+//   a `featureToRecord` function to convert a GeoJSON feature that we get from
 //   the client into a record that we can write to the database. Sometimes
 //   "deserializing" is referred to as "parsing".
 
@@ -79,18 +79,18 @@ const featureToRecord = function (feature) {
 
 
 // Routing Functions
-// ~~~~~~~~~~~~~~~~~
+// -----------------
 //
 // This API exposes (i.e., makes available) one route that can be used with two
 // different methods. It is common in REST-style APIs to have a single URL do
-// different things depending on the HTTP method used in the API request. A REST
-// API will determine the paths that it exposes by the "resources" that the API
-// needs to represent.
+// different things depending on the HTTP method used in the client's request.
 //
-// In this API, the main type of resource we're representing is a "trail issue",
-// and we want to allow API clients to (1) get a list of trail issues, and (2)
-// create a new trail issue. For this we'll expose a route at `/trail_issues/`,
-// and use the **GET** and **POST** HTTP methods, respectively.
+// A REST API will determine the paths that it exposes by the "resources" that
+// the API needs to represent. In this API, the main type of resource we're
+// representing is a "trail issue", and we want to allow API clients to (1) get
+// a list of trail issues, and (2) create a new trail issue. For this we'll
+// expose a route at `/trail_issues/`, and use the **GET** and **POST** HTTP
+// methods, respectively.
 //
 // Think of `/trail_issues/` as a resource that represents the collection of all
 // trail issues in the system (for our case, we're limiting ourselves to
@@ -130,7 +130,7 @@ app.post('/trail_issues/', (req, res) => {
 
 
 // Running the Server
-// ~~~~~~~~~~~~~~~~~~
+// ------------------
 //
 // If there is an environment variable named `PORT` then we will run the server
 // on the port number specified by that environment variable. Otherwise, we will
